a17f13c30a909a6f5dee868d0293554c94aec1a9
Shubo Chao <apiggy0204@gmail.com>
2018-05-25 08:44:54 +0800

    Demo of `onScroll` prop of WebView

--- App.js
+++ App.js
@@ -9,7 +9,8 @@ import {
   Platform,
   StyleSheet,
   Text,
-  View
+  View,
+  WebView,
 } from 'react-native';
 
 const instructions = Platform.select({
@@ -21,18 +22,54 @@ const instructions = Platform.select({
 
 type Props = {};
 export default class App extends Component<Props> {
+  constructor(props) {
+    super(props)
+
+    this.state = {
+      scrollY: 0,
+      scrollX: 0,
+      contentHeight: 0,
+      contentWidth: 0,
+      webviewHeight: 0,
+      webviewWidth: 0,
+      velocityY: 0,
+      velocityX: 0,
+    }
+
+    this.handleScroll = this.handleScroll.bind(this)
+  }
+
+  handleScroll(e) {
+    console.log(e.nativeEvent)
+
+    this.setState({
+      scrollY: e.nativeEvent.contentOffset.y,
+      scrollX: e.nativeEvent.contentOffset.x,
+      contentHeight: e.nativeEvent.contentSize.height,
+      contentWidth: e.nativeEvent.contentSize.width,
+      webviewHeight: e.nativeEvent.layoutMeasurement.height,
+      webviewWidth: e.nativeEvent.layoutMeasurement.width,
+      velocityY: e.nativeEvent.velocity.y,
+      velocityX: e.nativeEvent.velocity.x,
+    })
+  }
+
   render() {
     return (
       <View style={styles.container}>
-        <Text style={styles.welcome}>
-          Welcome to React Native!
-        </Text>
-        <Text style={styles.instructions}>
-          To get started, edit App.js
-        </Text>
-        <Text style={styles.instructions}>
-          {instructions}
-        </Text>
+        <Text style={styles.instructions}>scrollY: {this.state.scrollY}</Text>
+        <Text style={styles.instructions}>scrollX: {this.state.scrollX}</Text>
+        <Text style={styles.instructions}>contentHeight: {this.state.contentHeight}</Text>
+        <Text style={styles.instructions}>contentWidth: {this.state.contentWidth}</Text>
+        <Text style={styles.instructions}>webviewHeight: {this.state.webviewHeight}</Text>
+        <Text style={styles.instructions}>webviewWidth: {this.state.webviewWidth}</Text>
+        <Text style={styles.instructions}>velocityY: {this.state.velocityY}</Text>
+        <Text style={styles.instructions}>velocityX: {this.state.velocityX}</Text>
+        <WebView
+          source={{uri: 'https://github.com/facebook/react-native'}}
+          style={{flex: 1}}
+          onScroll={this.handleScroll}
+        />
       </View>
     );
   }
@@ -42,8 +79,6 @@ const styles = StyleSheet.create({
   container: {
     flex: 1,
     justifyContent: 'center',
-    alignItems: 'center',
-    backgroundColor: '#F5FCFF',
   },
   welcome: {
     fontSize: 20,
